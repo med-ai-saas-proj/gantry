69bf0977ace4b4f2dfa2f4dc00cf3e3c94adb40d
thai0125nt <thai0125nt@gmail.com>
2025-08-30 12:19:38 +0700

    feat: update

--- src/consts/env.py
+++ src/consts/env.py
@@ -5,6 +5,6 @@ class EnvConsts:
     CORE_DNS = os.environ["CORE_DNS"]
     CORE_MAX_CONN = int(os.environ.get("CORE_MAX_CONN", "1"))
     CORE_MIN_CONN = int(os.environ.get("CORE_MIN_CONN", "1"))
-    STAGE = os.environ["STAGE"]
+    STAGE = os.environ["STAGE"]  # PROD, DEV, LOCAL
     DEBUG = int(os.environ.get("DEBUG", "0"))
     GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,9 +1,44 @@
 import logging
+import time
+import orjson
+import structlog
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s:[%(levelname)s]:%(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+from src.consts.env import EnvConsts
 
-LOGGER = logging.getLogger(__name__)
+
+def orjson_renderer(_, __, event_dict):
+    return orjson.dumps(event_dict).decode()
+
+
+def ms_timestamper(_, __, event_dict):
+    event_dict["latencyMs"] = time.time_ns() // 1_000_000
+    return event_dict
+
+
+def configure_default_logging(env, logger: logging.Logger) -> structlog.stdlib.BoundLogger:
+    pre_chain = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.StackInfoRenderer(),
+        ms_timestamper,
+    ]
+    processors = pre_chain
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.INFO)
+
+    if env.lower() in ["prod", "dev"]:
+        processors += [orjson_renderer]
+    else:
+        processors += [structlog.dev.ConsoleRenderer()]
+
+    return structlog.wrap_logger(
+        logger,
+        processors=processors,
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        context_class=dict,
+        # logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+
+LOGGER = configure_default_logging(EnvConsts.STAGE, logging.getLogger("core"))
