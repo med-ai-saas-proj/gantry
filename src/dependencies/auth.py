7cfc9bfe5dfb87fb7a8f0ba3a07261d8bbee0eb7
thng292venera <thong@venerian.space>
2025-09-16 09:14:48 +0700

    [chore] Placeholder for auth dependency

--- src/dependencies/auth.py
+++ src/dependencies/auth.py
@@ -1,6 +1,16 @@
+from src.entities import User
+from datetime import datetime
+
+
 def get_current_user_form_apikey(api_key: str):
-    pass
+    now = datetime.now()
+    return User(
+        id="", email="example@example.com", createdAt=now, updated_at=now
+    )
 
 
-def get_current_user_form_cookies(api_key: str):
-    pass
+def get_current_user_form_cookies(cookie: str):
+    now = datetime.now()
+    return User(
+        id="", email="example@example.com", createdAt=now, updated_at=now
+    )
