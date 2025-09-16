ae203e2163d4aeb2f6bd72d54a30bf92fa07b0fe
thng292venera <thong@venerian.space>
2025-09-16 11:12:42 +0700

    [chore] Update auth interface

--- src/dependencies/auth.py
+++ src/dependencies/auth.py
@@ -1,5 +1,6 @@
 from src.entities import User
 from datetime import datetime
+from fastapi import Request
 
 
 def get_current_user_form_apikey(api_key: str):
@@ -9,8 +10,16 @@ def get_current_user_form_apikey(api_key: str):
     )
 
 
-def get_current_user_form_cookies(cookie: str):
+def get_current_user_form_session(session: str):
     now = datetime.now()
     return User(
         id="", email="example@example.com", createdAt=now, updated_at=now
     )
+
+
+def get_current_user(request: Request):
+    api_key = request.headers.get("Authorization", "").removeprefix("Bearer ")
+    session: str = request.cookies.get("session", "")
+    if session:
+        return get_current_user_form_session(session)
+    return get_current_user_form_apikey(api_key)
