85b84dbe41303856cfc09ba96a721a5335763f81
thng292venera <thong@venerian.space>
2025-09-25 19:49:52 +0700

    [chore] Update scripts to setup crawl4ai

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+export UV_ENV_FILE=.env
+uv run --no-sync crawl4ai-setup
+uv run --no-sync alembic upgrade head
 uv run uvicorn server:app --host 0.0.0.0 --port 8000 \
     --env-file .env \
     --log-config log-config.json \
