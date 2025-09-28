46e12772a8fcff86622e38559b57d3d13a83665a
thng292venera <thong@venerian.space>
2025-09-28 17:50:05 +0700

    [fix][deploy] Prod script env

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -3,5 +3,4 @@
 uv run --no-sync crawl4ai-setup
 uv run --no-sync alembic upgrade head
 uv run --no-sync uvicorn server:app --host 0.0.0.0 --port 8000 \
-    --env-file .env \
     --log-config log-config.json
