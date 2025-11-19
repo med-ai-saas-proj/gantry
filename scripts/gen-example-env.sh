62478307e8676f39381c6c7be7bc504f533f3a4f
thng292venera <thong@venerian.space>
2025-11-20 06:42:45 +0700

    chore: Added shortcut for launching scripts

---
+++ scripts/gen-example-env.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+UV_ENV_FILE=.env uv run scripts.gen-example-env $@
--- scripts/setup-test-account.py
+++ scripts/setup-test-account.py
@@ -1,33 +1,22 @@
-from src.auth import utils
-from src.db.postgres.initialize import CORE_DB_SESSION_SCOPE
-
 import asyncio
 from datetime import UTC, datetime, timedelta
 
-from src.auth.services.user import UserService
+from src.auth.factories import getUserService
 
 
-user_service = UserService(CORE_DB_SESSION_SCOPE)
+username = "test"
 email = "test@test.com"
 password = "Thisisaverystrongpassword123"
 
 
 async def main():
+    user_service = getUserService()
     try:
-        user = await user_service.register_user(email=email, password=password)
+        user = await user_service.emailRegister(username, email, password)
         print(f"Created a test account with {email=}, {password=}")
     except:
-        user = await user_service.get_user_by_email(email)
+        user = await user_service.emailLogin(email, password)
         print(f"Account already created {email=}, {password=}")
-    payload = {
-        "user_id": str(user["id"]),
-        "email": user["email"],
-        "exp": datetime.now(UTC) + timedelta(days=365),
-        "iat": datetime.now(UTC),
-    }
-
-    api_key = utils.create_token(payload)
-    print(f"Created an API key: {api_key=}")
 
 
 if __name__ == "__main__":
