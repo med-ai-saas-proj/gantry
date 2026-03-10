0acc979328c89ce2817eb7597d78ba7d598a0fba
cutehusky <liyihong102@gmail.com>
2026-03-10 17:01:03 +0700

    feat: enhance OpenTelemetry configuration with support for Prometheus and OTLP exporters

--- src/otel/settings.py
+++ src/otel/settings.py
@@ -1,13 +1,54 @@
+from enum import Enum
 from functools import lru_cache
 
 from pydantic import HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class TracesType(str, Enum):
+    otlp = "otlp"
+    disabled = "disabled"
+
+class MetricsType(str, Enum):
+    otlp = "otlp"
+    prometheus = "prometheus"
+    disabled = "disabled"
+
+class LogsType(str, Enum):
+    otlp = "otlp"
+    disabled = "disabled"
+
+class ExporterProtocol(str, Enum):
+    grpc = "grpc"
+    http_protobuf = "http/protobuf"
+
+
 class OtelSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="otel_", case_sensitive=False)
 
-    exporter_otlp_endpoint: HttpUrl
+    exporter_otlp_endpoint: HttpUrl = "http://localhost:4317"
+    exporter_otlp_protocol: ExporterProtocol = ExporterProtocol.grpc
+
+    traces: TracesType = "otlp"
+    metrics: MetricsType = "otlp"
+    logs: LogsType = "otlp"
+
+    prometheus_port: int = 8001
+
+    # OTEL defaults envvars
+    # Traces
+    # - :envvar:`OTEL_BSP_SCHEDULE_DELAY`
+    # - :envvar:`OTEL_BSP_MAX_QUEUE_SIZE`
+    # - :envvar:`OTEL_BSP_MAX_EXPORT_BATCH_SIZE`
+    # - :envvar:`OTEL_BSP_EXPORT_TIMEOUT`
+    # Metrics
+    # -: envvar:`OTEL_METRIC_EXPORT_TIMEOUT`
+    # -: envvar:`OTEL_METRIC_EXPORT_INTERVAL`
+    # Logs
+    # -: envvar:`OTEL_BLRP_SCHEDULE_DELAY`
+    # -: envvar:`OTEL_BLRP_MAX_QUEUE_SIZE`
+    # -: envvar:`OTEL_BLRP_MAX_EXPORT_BATCH_SIZE`
+    # -: envvar:`OTEL_BLRP_EXPORT_TIMEOUT
 
 
 @lru_cache(1)
--- src/otel/setup.py
+++ src/otel/setup.py
@@ -1,4 +1,10 @@
-from .settings import getOtelSettings
+from .settings import (
+    LogsType,
+    TracesType,
+    MetricsType,
+    ExporterProtocol,
+    getOtelSettings,
+)
 
 import logging
 
@@ -18,19 +24,15 @@ from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
 from opentelemetry.instrumentation.system_metrics import (
     SystemMetricsInstrumentor,
 )
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
-    OTLPSpanExporter,
-)
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
-    OTLPMetricExporter,
-)
 
 
 otel_settings = getOtelSettings()
 
-
-def setupOtel(service_name: str, service_version: str, logger: BoundLogger):
+def setupOtel(
+    service_name: str,
+    service_version: str,
+    logger: BoundLogger,
+):
     resource = Resource.create(
         {
             "service.name": service_name,
@@ -38,46 +40,90 @@ def setupOtel(service_name: str, service_version: str, logger: BoundLogger):
         }
     )
 
-    tracer_provider = TracerProvider(resource=resource)
-    tracer_provider.add_span_processor(
-        BatchSpanProcessor(
-            OTLPSpanExporter(
-                endpoint=otel_settings.exporter_otlp_endpoint.encoded_string(),
-                insecure=True,
+    if otel_settings.traces == TracesType.otlp:
+        if otel_settings.exporter_otlp_protocol == ExporterProtocol.grpc:
+            from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+                OTLPSpanExporter,
             )
+        elif otel_settings.exporter_otlp_protocol == ExporterProtocol.http_protobuf:
+            from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+                OTLPSpanExporter,
+            )
+        else:
+            raise ValueError(
+                f"Unsupported OTLP exporter protocol: {otel_settings.exporter_otlp_protocol}"
+            )
+        exporter = OTLPSpanExporter(
+            endpoint=otel_settings.exporter_otlp_endpoint.encoded_string(),
+            insecure=True,
         )
-    )
-    trace.set_tracer_provider(tracer_provider)
+        tracer_provider = TracerProvider(resource=resource)
+        tracer_provider.add_span_processor(BatchSpanProcessor(exporter))
+        trace.set_tracer_provider(tracer_provider)
 
-    meter_provider = MeterProvider(
-        resource=resource,
-        metric_readers=[
-            PeriodicExportingMetricReader(
-                OTLPMetricExporter(
-                    endpoint=otel_settings.exporter_otlp_endpoint.encoded_string(),
-                    insecure=True,
-                )
+    if otel_settings.metrics == MetricsType.otlp:
+        if otel_settings.exporter_otlp_protocol == ExporterProtocol.grpc:
+            from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
+                OTLPMetricExporter,
             )
-        ],
-    )
-    metrics.set_meter_provider(meter_provider)
+        elif otel_settings.exporter_otlp_protocol == ExporterProtocol.http_protobuf:
+            from opentelemetry.exporter.otlp.proto.http.metric_exporter import (
+                OTLPMetricExporter,
+            )
+        else:
+            raise ValueError(
+                f"Unsupported OTLP exporter protocol: {otel_settings.exporter_otlp_protocol}"
+            )
 
-    logger_provider = LoggerProvider(resource=resource)
-    logger_provider.add_log_record_processor(
-        BatchLogRecordProcessor(
-            OTLPLogExporter(
-                endpoint=otel_settings.exporter_otlp_endpoint.encoded_string(),
-                insecure=True,
-            )
+        metric_exporter = OTLPMetricExporter(
+            endpoint=otel_settings.exporter_otlp_endpoint.encoded_string(),
+            insecure=True,
         )
-    )
-    _logs.set_logger_provider(logger_provider)
+        meter_provider = MeterProvider(
+            resource=resource,
+            metric_readers=[
+                PeriodicExportingMetricReader(metric_exporter)
+            ],
+        )
+        metrics.set_meter_provider(meter_provider)
+    elif otel_settings.metrics == MetricsType.prometheus:
+        from prometheus_client import start_http_server
+        from opentelemetry.exporter.prometheus import PrometheusMetricReader
 
-    handler = LoggingHandler(
-        level=logging.NOTSET, logger_provider=logger_provider
-    )
-    logging.getLogger().addHandler(handler)  # root logger
-    logging.getLogger("uvicorn").addHandler(handler)  # uvicorn logger
+        meter_provider = MeterProvider(
+            resource=resource,
+            metric_readers=[PrometheusMetricReader()],
+        )
+        metrics.set_meter_provider(meter_provider)
+        start_http_server(port=otel_settings.prometheus_port, addr="0.0.0.0")
+
+    if otel_settings.logs == LogsType.otlp:
+        if otel_settings.exporter_otlp_protocol == ExporterProtocol.grpc:
+            from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
+                OTLPLogExporter,
+            )
+        elif otel_settings.exporter_otlp_protocol == ExporterProtocol.http_protobuf:
+            from opentelemetry.exporter.otlp.proto.http._log_exporter import (
+                OTLPLogExporter,
+            )
+        else:
+            raise ValueError(
+                f"Unsupported OTLP exporter protocol: {otel_settings.exporter_otlp_protocol}"
+            )
+
+        exporter = OTLPLogExporter(
+            endpoint=otel_settings.exporter_otlp_endpoint.encoded_string(),
+            insecure=True,
+        )
+        logger_provider = LoggerProvider(resource=resource)
+        logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+        _logs.set_logger_provider(logger_provider)
+
+        handler = LoggingHandler(
+            level=logging.NOTSET, logger_provider=logger_provider
+        )
+        logging.getLogger().addHandler(handler)  # root logger
+        logging.getLogger("uvicorn").addHandler(handler)  # uvicorn logger
 
     logger.info(f"OpenTelemetry initialized for service: {service_name}")
 
