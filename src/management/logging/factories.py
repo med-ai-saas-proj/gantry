d1341af675b29f68d1ff96338fa67e7394d75809
cutehusky <liyihong102@gmail.com>
2026-03-17 13:55:05 +0700

    feat: add logging settings and update LogQueryService to use dynamic Loki URL

--- src/management/logging/factories.py
+++ src/management/logging/factories.py
@@ -1,12 +1,15 @@
 """Factory functions for creating service instances."""
-import httpx
-
 from .services import LogQueryService
+from .settings import getLoggingSettings
+
 from functools import lru_cache
+
+import httpx
 
 
 @lru_cache(1)
 def getLogQueryService() -> LogQueryService:
     """Get a singleton instance of LogQueryService."""
-    http_client = httpx.Client(base_url="http://localhost:3100")
+    logging_settings = getLoggingSettings()
+    http_client = httpx.Client(base_url=str(logging_settings.loki_url))
     return LogQueryService(http_client)
