c5128d685831347bf11d96e95f2a067d298b9209
cutehusky <liyihong102@gmail.com>
2026-03-18 17:23:29 +0700

    feat: update QueryLogRequest to use datetime for start and end fields and enhance log query descriptions

--- src/management/logging/routers.py
+++ src/management/logging/routers.py
@@ -4,30 +4,27 @@ from src.shared.settings import getAppSetting
 from .dtos import QueryLogRequest
 from .factories import getLogQueryService
 
-import datetime
 from typing import Literal, Annotated
+from datetime import datetime
 
 from fastapi import Depends, APIRouter
 from fastapi.params import Body, Query
 
 
-logging_router = APIRouter(prefix="/logging")
+logging_router = APIRouter(prefix="/logging", tags=["logging"])
 
 log_query_service = getLogQueryService()
 app_settings = getAppSetting()
 
 
-@logging_router.get("/")
-async def query_log(
+@logging_router.get(
+    "/",
+    description="Query logs with simple query parameters, suitable for simple queries with only label filters and keyword search. For more complex queries that cannot be easily expressed in query parameters, please use the POST endpoint with a request body."
+)
+async def simple_query_log(
     user_info: Annotated[UserInfo, Depends(getUserInfo)],
-    start: int | float | datetime.datetime = Query(
-        ...,
-        description="int is epoch time in seconds, float is epoch time in seconds",
-    ),
-    end: int | float | datetime.datetime = Query(
-        ...,
-        description="int is epoch time in seconds, float is epoch time in seconds",
-    ),
+    start: datetime = Query(...),
+    end: datetime = Query(...),
     limit: int = Query(default=1000, le=10000, gt=0),
     direction: Literal["forward", "backward"] = Query(default="backward"),
     level: Literal["debug", "info", "warn", "error"] | None = Query(
@@ -62,8 +59,11 @@ async def query_log(
     return res.unwrap()
 
 
-@logging_router.post("/")
-async def search_log(
+@logging_router.post(
+    "/",
+    description="Query logs with more complex request body, supports keyword search and filters with operators (e.g. >, <, =) and custom Loki Label Filters through custom_query field. The GET endpoint is more suitable for simple queries with only label filters and no keyword search, while this POST endpoint can handle more complex queries that cannot be easily expressed in query parameters."
+)
+async def query_log(
     user_info: Annotated[UserInfo, Depends(getUserInfo)],
     query_request: Annotated[QueryLogRequest, Body()],
 ) -> list[dict]:
