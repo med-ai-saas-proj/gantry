f2579c2bf391863dfb26b6d1c8ce6099a5295ae7
Nguyen Thanh Nam <namnguyen160504@gmail.com>
2026-03-29 19:39:15 +0700

    chore: fix head alembic id

--- alembic/versions/b7c8d9e0f1a2_add_project_schema.py
+++ alembic/versions/b7c8d9e0f1a2_add_project_schema.py
@@ -1,7 +1,7 @@
 """add_project_schema
 
 Revision ID: b7c8d9e0f1a2
-Revises: a1b2c3d4e5f6
+Revises: 323163f80297
 Create Date: 2026-03-08 12:00:00.000000
 Feature: Project
 Reason: Add Project schema and convert Organization.Settings.extra to JSONB
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # revision identifiers, used by Alembic.
 revision: str = "b7c8d9e0f1a2"
-down_revision: Union[str, Sequence[str], None] = "a1b2c3d4e5f6"
+down_revision: Union[str, Sequence[str], None] = "323163f80297"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
