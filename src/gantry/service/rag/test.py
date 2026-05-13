f1a4157b671806ad6dc671f11680c77790978112
Nguyen Thanh Nam <namnguyen160504@gmail.com>
2026-05-13 20:06:31 +0700

    feat: add more test types

--- src/gantry/service/rag/test.py
+++ src/gantry/service/rag/test.py
@@ -1,8 +1,7 @@
 """Unit tests for project DTO validation."""
 
-from gantry.db.factories import getSessionManager
 from gantry.settings.rag import VectorOpsType, VectorIndexType
-from gantry.service.utils.rag.utils import getTableName, get_orm_class
+from gantry.service.rag.utils import getTableName, get_orm_class
 
 import unittest
 
@@ -21,15 +20,17 @@ class TestRagInsert(unittest.IsolatedAsyncioTestCase):
             }
         )
         EmbeddingModel = get_orm_class(table_name, 1536)
-        async with getSessionManager().get_session() as session:
-            new_record = EmbeddingModel(
-                embedding=range(1536),
-                file_id=0,
-                text="sample text",
-                project_id=0,
-            )
-            session.add(new_record)
-            await session.commit()
+        new_record = EmbeddingModel(
+            embedding=range(1536),
+            file_id=0,
+            text="sample text",
+            project_id=0,
+        )
+
+        self.assertEqual(EmbeddingModel.__table__.name, table_name)
+        self.assertEqual(new_record.file_id, 0)
+        self.assertEqual(new_record.text, "sample text")
+        self.assertEqual(new_record.project_id, 0)
 
 
 if __name__ == "__main__":
